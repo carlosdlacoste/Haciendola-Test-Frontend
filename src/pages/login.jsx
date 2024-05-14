307b144315d1ab29f3fcc6291c6f206f052cc9d0
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-14 19:20:12 -0400

    Solicitud de autenticacion hecha satisfactoriamente desde el frontend

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -10,8 +10,21 @@ export const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-        // Lógica de autenticación...
-        // navigate('/products');
+            const response = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                localStorage.setItem('token', data.token);
+                navigate('/');
+            } else {
+                setError('Credenciales inválidas. Por favor, inténtalo de nuevo.');
+            }
         } catch (error) {
         setError('Credenciales inválidas. Por favor, inténtalo de nuevo.');
         }
@@ -21,17 +34,17 @@ export const Login = () => {
         <div className="container mt-5">
         <h2 className="mb-4">Iniciar Sesión</h2>
         {error && <div className="alert alert-danger">{error}</div>}
-        <form onSubmit={handleLogin}>
+        <div>
             <div className="mb-3">
             <label htmlFor="username" className="form-label">Nombre de Usuario</label>
-            <input type="text" className="form-control" id="username" placeholder="Ingrese su nombre de usuario" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" className="form-control" id="username" placeholder="Ingrese su nombre de usuario" value={username || ''} onChange={(e) => setUsername(e.target.value)} />
             </div>
             <div className="mb-3">
             <label htmlFor="password" className="form-label">Contraseña</label>
-            <input type="password" className="form-control" id="password" placeholder="Ingrese su contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" className="form-control" id="password" placeholder="Ingrese su contraseña" value={password || ''} onChange={(e) => setPassword(e.target.value)} />
             </div>
-            <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
-        </form>
+            <button type="button" onClick={(e) => handleLogin(e)} className="btn btn-primary">Iniciar Sesión</button>
+        </div>
         </div>
     );
 };
