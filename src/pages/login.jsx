72ffb7fe9c643f2d8edfe24e85549c963626aa71
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-15 13:27:16 -0400

    Ajustado form login al medio de la pantalla para una mejor apariencia

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -33,20 +33,24 @@ export const Login = () => {
     };
 
     return (
-        <div className="container mt-5">
-        <h2 className="mb-4">Iniciar Sesión</h2>
-        {error && <div className="alert alert-danger">{error}</div>}
-        <div>
-            <div className="mb-3">
-            <label htmlFor="username" className="form-label">Nombre de Usuario</label>
-            <input type="text" className="form-control" id="username" placeholder="Ingrese su nombre de usuario" value={username || ''} onChange={(e) => setUsername(e.target.value)} />
+        <div className="container mt-5 d-flex justify-content-center align-items-center vh-100 flex-column">
+            <div className='w-50'>
+                <h2 className="mb-4 text-center">Iniciar Sesión</h2>
+                {error && <div className="alert alert-danger">{error}</div>}
+                <div>
+                    <div className="mb-3">
+                        <label htmlFor="username" className="form-label fw-bold">Nombre de Usuario</label>
+                        <input type="text" className="form-control" id="username" placeholder="Ingrese su nombre de usuario" value={username || ''} onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label fw-bold">Contraseña</label>
+                        <input type="password" className="form-control" id="password" placeholder="Ingrese su contraseña" value={password || ''} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className='d-flex justify-content-center'>
+                        <button type="button" onClick={(e) => handleLogin(e)} className="btn btn-primary w-75">Iniciar Sesión</button>
+                    </div>
+                </div>
             </div>
-            <div className="mb-3">
-            <label htmlFor="password" className="form-label">Contraseña</label>
-            <input type="password" className="form-control" id="password" placeholder="Ingrese su contraseña" value={password || ''} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <button type="button" onClick={(e) => handleLogin(e)} className="btn btn-primary">Iniciar Sesión</button>
-        </div>
         </div>
     );
 };
