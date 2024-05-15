af921727321a1d839afa98e12a136b2fb8db74e1
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-15 12:29:31 -0400

    Depurado el codigo en pagina home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import {useNavigate} from "react-router-dom";
+import React from 'react';
 import { useAuth } from '../store/authContext';
 import { Login } from './login';
 import { Products } from './products';
